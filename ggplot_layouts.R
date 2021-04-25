f437610bfa06ca3f696580b6abfce6acb3a229ad
BenSolomon <bds1217@gmail.com>
2021-04-24 18:40:22 -0700

    started 2_agreement

--- ggplot_layouts.R
+++ ggplot_layouts.R
@@ -4,11 +4,11 @@ library(ggh4x)
 
 gg_accuracy <- function(df){
   df %>% 
-    ggplot(aes(x = genotyper, y = accuracy))+
+    ggplot(aes(x = field, y = accuracy))+
     stat_summary(fun = mean, geom = "point", position = position_dodge(width=0.9)) +
     stat_summary(fun.data = mean_se, geom = "errorbar", position=position_dodge(width=0.9)) +
     theme_bw() +
-    facet_nested(.~locus+field, scales = "free_y") +
+    facet_nested(.~locus+genotyper, scales = "free_y") +
     coord_cartesian(ylim = c(0,1))+
     scale_y_continuous(n.breaks = 6)+
     labs(y = "Accuracy", x = "")+
