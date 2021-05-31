0cf19d3996ba65699c4455e5c13968762edf23c0
BenSolomon <bds1217@gmail.com>
2021-05-30 20:42:59 -0700

    Update compare_hla and allele_match to better reflect success/accuracy distinction

--- calculation_functions.R
+++ calculation_functions.R
@@ -24,7 +24,7 @@ allele_match <- function(ref,comp,ground_truth=T, verbose=F){
     
     
     # If comparing to a ground_truth, only penalize NAs in comp, not those in ref
-    if (n_ref == 1 & ground_truth == T){return(sum(any(ref == comp, na.rm = T)))}
+    if (n_ref == 1 & ground_truth == T){return(sum(any(ref[!is.na(ref)] %in% comp, na.rm = T)))}
     
     # If unequal number of NAs between ref and comp, fill smaller with random placeholder
     ## If both genotypes only have one allele, don't want to fill with random placeholder
@@ -103,14 +103,15 @@ allele_match <- function(ref,comp,ground_truth=T, verbose=F){
 # unit_test_output
 
 
-compare_hla<- function(hla_df, reference = "invitro", exclude_missing=T, compare_function = "accuracy", na_drop = T){
+compare_hla<- function(hla_df, reference = "invitro", exclude_missing=T, penalize_extra_drb345 = T, penalize_extra_classic = F){
   # Input checks
   arg_col <- makeAssertCollection()
   hla_df_columns <- c("sample", "allele_id", "locus", "field_1", "field_2", "field_3", "genotyper")
   assertNames(names(hla_df), permutation.of = hla_df_columns, .var.name = "hla_df column names", add = arg_col)
   assertChoice(reference, unique(hla_df$genotyper), add = arg_col)
   assertLogical(exclude_missing, add = arg_col)
-  assertChoice(compare_function, c("accuracy", "agreement"), add = arg_col)
+  assertLogical(penalize_extra_drb345, add = arg_col)
+  assertLogical(penalize_extra_classic, add = arg_col)
   if (arg_col$isEmpty()==F) {map(arg_col$getMessages(),print);reportAssertions(arg_col)}
   
   # Function
@@ -123,21 +124,26 @@ compare_hla<- function(hla_df, reference = "invitro", exclude_missing=T, compare
     dplyr::rename("reference" = contains(reference)) %>%
     pivot_longer(contains("genotyper"),  names_to = "genotyper", values_to = "allele",names_prefix = "genotyper_")
   if (exclude_missing == T){
-    df <- df %>% 
-      filter_at(vars(c(reference, allele)), function(x) !is.na(x)) %>% 
-      mutate(n_reference = map_dbl(reference, function(x) sum(!is.na(x) & !grepl("NA", x)))) %>% 
-      mutate(n_allele = map_dbl(allele, function(x) sum(!is.na(x) & !grepl("NA", x)))) %>% 
+    df <- df %>%
+      filter_at(vars(c(reference, allele)), function(x) !is.na(x))
+      # mutate(n_reference = map_dbl(reference, function(x) sum(!is.na(x) & !grepl("NA", x)))) %>%
+      # mutate(n_allele = map_dbl(allele, function(x) sum(!is.na(x) & !grepl("NA", x))))
       # filter_at(vars(contains("n_")), function(x) x ==2) # Removed b/c excluding samples where invitro n = 1 and sample n = 2, which is unwanted
-      filter(n_allele == 2)
+      # filter(n_allele == 2)
   }
-  if (compare_function == "accuracy"){
-    df <- df %>% mutate(accuracy = map2_dbl(reference, allele, allele_match))
-  } else if (compare_function == "agreement"){
-    df %>% mutate(accuracy = map2_dbl(reference, allele, allele_agreement))
-  } 
-  if (na_drop == T){
-    df <- df %>% drop_na()
-  }
+  df_drb345 <- df %>% 
+    filter(grepl("^DRB[345]",locus)) %>% 
+    mutate(accuracy = map2_dbl(reference, allele, allele_match, ground_truth = !penalize_extra_drb345))
+  df_other <- df %>% 
+    filter(grepl("^[ABC]",locus) | grepl("^D[PQR][AB]1", locus)) %>% 
+    mutate(accuracy = map2_dbl(reference, allele, allele_match, ground_truth = !penalize_extra_classic))
+  df <- bind_rows(
+    df_other,
+    df_drb345
+  )
+  # if (na_drop == T){
+  #   df <- df %>% drop_na()
+  # }
   df
 }
 # compare_hla(hla_df = all_hla, reference = "invitro", exclude_missing = T, compare_function = "accuracy")
